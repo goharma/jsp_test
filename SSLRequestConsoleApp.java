7ebe4359aad4754039a588d9e374ec0029eb68ad
blinkesto <3542038+goharma@users.noreply.github.com>
2025-03-26 15:37:06 -0500

    Update SSLRequestConsoleApp.java

--- SSLRequestConsoleApp.java
+++ SSLRequestConsoleApp.java
@@ -66,4 +66,23 @@ public class SSLRequestConsoleApp {
 
     // Method to disable SSL validation and allow retrieval of invalid certificates
     private static void disableSSLValidation() throws Exception {
-        TrustManager[] trustAllCertifica
+        TrustManager[] trustAllCertificates = new TrustManager[]{
+            new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCertificates, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+        // Disable hostname verification
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+    }
+}
